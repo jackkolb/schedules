b4dac0b6edba99deda0e986c66d47f170571e2ec
Jack Kolb <jackhunterkolb@gmail.com>
2019-11-09 15:38:09 -0800

    encapsulated app.run

--- app.py
+++ app.py
@@ -82,4 +82,5 @@ def add_headers(response):
 if app.debug:
     app.after_request(add_headers)
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
