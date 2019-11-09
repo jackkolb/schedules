6a31b44c6847c00c86c6335362927e755ffa74a6
EdCarrasco <carrasco.edw@gmail.com>
2019-11-09 15:10:43 -0800

    finished first pass at scheduler

--- js/main.js
+++ js/main.js
@@ -2,110 +2,149 @@ let elemOnMousedown = null
 let elemOnMouseover = null
 let selectionType = null
 let selectedCells = []
+let cells = []
 
 function fillTable(table, hours, days) {
-	let titleRow = createTitleRow(days)
-	table.append(titleRow)
-	for (let row = 0; row < hours.length; row++) {
-		let hourRow = createHourRow(row, hours[row], days)
-		table.append(hourRow)
-	}
+    let titleRow = createTitleRow(days)
+    table.append(titleRow)
+    for (let row = 0; row < hours.length; row++) {
+        let hourRow = createHourRow(row, hours[row], days)
+        table.append(hourRow)
+    }
 }
 
 function createTitleRow(days) {
-	let tr = document.createElement('tr')
-	let th = document.createElement('th')
-	tr.append(th)
-	for (let day of days) {
-		let th = document.createElement('th')
-		th.innerHTML = day.substring(0,3)
-		tr.append(th)
-	}
-	return tr
+    let tr = document.createElement('tr')
+    let th = document.createElement('th')
+    tr.append(th)
+    for (let day of days) {
+        let th = document.createElement('th')
+        th.innerHTML = day.substring(0,3)
+        tr.append(th)
+    }
+    return tr
 }
 
 function createHourRow(row, hour, days) {
-	let tr = document.createElement('tr')
-	let td = document.createElement('td')
-	td.innerHTML = hour
-	td.classList.add('no-user-select')
-	tr.append(td)
-	for (let col = 0; col < days.length; col++) {
-		let cell = createCell(row, col)
-		tr.append(cell)
-	}
-	return tr
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    td.innerHTML = hour
+    td.classList.add('no-user-select')
+    tr.append(td)
+    for (let col = 0; col < days.length; col++) {
+        let cell = createCell(row, col)
+        tr.append(cell)
+    }
+    return tr
+}
+
+function cellOnMousedown (event) {
+    elemOnMousedown = event.target
+
+    const state = event.target.getAttribute('data-state')
+    if (Number(state) === 1) {
+    setCell(event.target, 0)
+        selectionType = 0
+    } else if (Number(state) === 0) {
+        setCell(event.target, 1)
+        selectionType = 1
+    }
+    selectedCells.push(event.target)
 }
 
 function createCell(row, col) {
-	let td = document.createElement('td')
-	td.innerHTML = "("+row+","+col+")"
-	td.classList.add('schedule-cell')
-	td.classList.add('no-user-select')
+    let td = document.createElement('td')
+    td.innerHTML = "("+row+","+col+")"
+    td.classList.add('schedule-cell')
+    td.classList.add('no-user-select')
 
-	td.setAttribute('data-state', 0)
-	td.setAttribute('data-row', row)
-	td.setAttribute('data-col', col)
+    td.setAttribute('data-state', 0)
+    td.setAttribute('data-row', row)
+    td.setAttribute('data-col', col)
 
-	td.addEventListener("mousedown", (event) => {
-		elemOnMousedown = event.target
-	})
+    td.addEventListener("mousedown", cellOnMousedown)
 
-	td.addEventListener("mouseover", (event) => {
-		elemOnMouseover = event.target
-	})
-	return td
+    td.addEventListener("mouseover", (event) => {
+        elemOnMouseover = event.target
+    })
+    return td
+}
+
+function setCell(elem, state) {
+    elem.setAttribute('data-state', state)
+    //elem.innerHTML = state
+    paintCell(elem, state)
+}
+
+function paintCell(elem, state) {
+    if (Number(state) === 1) {
+        elem.classList.remove('cell-red')
+        elem.classList.add('cell-green')
+    } else if (Number(state) === 0) {
+    elem.classList.remove('cell-green')
+    elem.classList.add('cell-red')
+    } else {
+        console.error('Invalid state')
+    }
 }
 
 function paintMouseoverCells(event) {
-	console.log("\nelemOnMousedown")
-	console.log(elemOnMousedown)
+    console.log(elemOnMousedown)
+    if (elemOnMousedown) {
+        const mouseDownRow = elemOnMousedown.getAttribute('data-row')
+        const mouseDownCol = elemOnMousedown.getAttribute('data-col')
+        const mouseOverRow = elemOnMouseover.getAttribute('data-row')
+        const mouseOverCol = elemOnMouseover.getAttribute('data-col')
 
-	console.log("elemOnMouseover")
-	console.log(elemOnMouseover)
+        const highRow = Math.max(mouseDownRow, mouseOverRow)
+        const lowRow  = Math.min(mouseDownRow, mouseOverRow)
+        const highCol = Math.max(mouseDownCol, mouseOverCol)
+        const lowCol  = Math.min(mouseDownCol, mouseOverCol)
 
-	if (!elemOnMousedown) {
-		return;
-	}
+        
+        
+        selectedCells = []
+        for (let cell of cells) {
+            const state = cell.getAttribute('data-state')
+            const row = cell.getAttribute('data-row')
+            const col = cell.getAttribute('data-col')
 
-	const mouseDownRow = elemOnMousedown.getAttribute('data-row')
-    const mouseDownCol = elemOnMousedown.getAttribute('data-col')
-    const mouseOverRow = elemOnMouseover.getAttribute('data-row')
-    const mouseOverCol = elemOnMouseover.getAttribute('data-col')
-
-	const highRow = Math.max(mouseDownRow, mouseOverRow)
-    const lowRow  = Math.min(mouseDownRow, mouseOverRow)
-    const highCol = Math.max(mouseDownCol, mouseOverCol)
-    const lowCol  = Math.min(mouseDownCol, mouseOverCol)
-
-	let cells = document.getElementsByClassName('schedule-cell')
-	let selectedCells = []
-
-	for (let cell of cells) {
-		const state = cell.getAttribute('data-state')
-		const row = cell.getAttribute('data-row')
-		const col = cell.getAttribute('data-col')
-
-		if (row >= lowRow && row <= highRow && col >= lowCol && col <= highCol) {
-			selectedCells.push(cell)
-		}
-	}
+            if (row >= lowRow && row <= highRow && col >= lowCol && col <= highCol) {
+                selectedCells.push(cell)
+                paintCell(cell, selectionType)
+            } else {
+                paintCell(cell, state)
+            }
+        }
+    }
 }
 
 
+
+
 function main(table) {
-	//const aggregateTable = document.getElementById('table-display')
-	//const toggleCells = document.getElementsByClassName('cell')
-	//const aggregateCells = document.getElementsByClassName('cell-display')
-	
+    //const aggregateTable = document.getElementById('table-display')
+    //const toggleCells = document.getElementsByClassName('cell')
+    //const aggregateCells = document.getElementsByClassName('cell-display')
+    
 
-	const hours = ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', ' 1:00', ' 1:30', '2:00', '2:30', '3:00']
-	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const hours = ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', ' 1:00', ' 1:30', '2:00', '2:30', '3:00']
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-	fillTable(table, hours, days)
+    fillTable(table, hours, days)
+    cells = document.getElementsByClassName('schedule-cell')
 
-	document.addEventListener("mouseover", paintMouseoverCells)
-	document.addEventListener("mousedown", paintMouseoverCells)
+    document.addEventListener("mouseover", paintMouseoverCells)
+    document.addEventListener("mousedown", paintMouseoverCells)
+    document.addEventListener("mouseup", (event) => {
+        for (let cell of selectedCells) {
+            setCell(cell, selectionType)
+        }
+
+        selectedCells = []
+        elemOnMousedown = null
+        selectionType = null
+    })
 }
 
 const scheduleTable = document.getElementById('schedule-table')
@@ -113,9 +152,9 @@ main(scheduleTable)
 
 // let xhttp = new XMLHttpRequest();
 // xhttp.onreadystatechange = () => {
-// 	if (this.readyState == 4 && this.status == 200) {
-// 		console.log("request!")
-// 	}
+//  if (this.readyState == 4 && this.status == 200) {
+//      console.log("request!")
+//  }
 // }
 
 // const FIREBASE_URL = "htt10.28.92.95/org/data/?id=1"
