505308e61c6765bc6c3376ab46fccf81f8015487
Jack Kolb <jackhunterkolb@gmail.com>
2019-11-12 20:52:41 -0800

    attempt to add cors

--- main.py
+++ main.py
@@ -85,5 +85,10 @@ def update():
 
     return "success"
 
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
